dc188e8b6bbc3a95151376f2be614a53de45bd6b
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-06 20:11:05 +0000

    demo

--- connection.py
+++ connection.py
@@ -1,4 +1,6 @@
 from psycopg import OperationalError, connect
+from pprint import pprint
+
 
 def create_connection(db_name, db_user, db_password, db_host = "localhost", db_port = "5432"):
     connection = None
@@ -15,18 +17,22 @@ def create_connection(db_name, db_user, db_password, db_host = "localhost", db_p
         print(f"The error '{e}' occurred")
     return connection
 
+
+
 # Context managers - python
 # open vs. closed to DBs - need to close
 def select_all(query):
     # Connect to an existing database
     with create_connection("postgres", "postgres", "postgres") as conn:
         # Execute the query and fethc all records
+        pprint(conn.execute(query).fetchall())
         return conn.execute(query).fetchall()
 
 def select_one(query):
     # Connect to an existing database
     with create_connection("postgres", "postgres", "postgres") as conn:
         # Execute the query and fethc first record matching select
+        pprint(conn.execute(query).fetchone())
         return conn.execute(query).fetchone()
 
 def delete(query):
@@ -36,9 +42,16 @@ def delete(query):
         conn.execute(query)
         # Commit the query to the database for reals
         conn.commit()
+        pprint(conn.commit())
 
 def create(query):
     with create_connection("postgres", "postgres", "postgres") as conn:
         conn.execute(query)
         conn.commit()
+        pprint(conn.commit())
 
+def update(query):
+    with create_connection("postgres", "postgres", "postgres") as conn:
+        conn.execute(query)
+        conn.commit()
+        pprint(conn.commit())
--- scripts/demo.py
+++ scripts/demo.py
@@ -1,5 +1,7 @@
 # This is why you'll execute a series of SQL statements during demo day.
-from connection import select_all
+import sys
+sys.path.append("/workspace/sql-heroes")
+from connection import *
 
 # select_heroes = """
 #     SELECT * FROM heroes
@@ -11,17 +13,13 @@ from connection import select_all
 #     print(hero[1])
 
 
-# new_table = ("""
-#             CREATE TABLE newtable (
-#                 id serial PRIMARY KEY,
-#                 num integer,
-#                 data text)
-#             """)
 
 
-select_all('SELECT * FROM heroes')
-create('CREATE TABLE jordan(id int generated always as identity, num int, data text, PRIMARY KEY(id))')
+# test_one = 'SELECT * FROM heroes'
+# select_all(test_one)
 
+# create('CREATE TABLE super_vehicle(id int generated always as identity,  int, data text, PRIMARY KEY(id))')
+# delete('DROP TABLE jordan')
 
 
 # '''
@@ -36,17 +34,16 @@ create('CREATE TABLE jordan(id int generated always as identity, num int, data t
 # WHERE id = 2;
 # '''
 
-
-create_script = ''' CREATE TABLE IF NOT EXISTS jordan (
-    id int generated always as identity
-    name varchar NOT NULL,
-    somestring varchar,
-    somenumber int,
-    PRIMARY KEY(id)
-) '''
-
 # insert_script = '''INSERT INTO -tableName- (default, name, somestring, somenumber) VALUES (%s, %s, %s, %s)'''
 # insert_values = [('Jordan', 'This is a string', 3), ('Duff', 'This is also a string', 4)]
 # for record in insert_values:
 #     curr.execute(insert_script, record)
 
+
+# select_all('SELECT * FROM heroes')
+# heroes = 'SELECT * FROM heroes'
+# for name in heroes:
+#     print(name)
+
+
+select_all('SELECT * FROM heroes')
